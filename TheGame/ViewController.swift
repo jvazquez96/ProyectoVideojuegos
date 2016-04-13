5879e2524a3089af32ee383683953be5e60d1c4a
Jorge Vázquez <jorge.armando@icloud.com>
2016-04-13 12:58:22 -0500

    Initial commit

---
+++ TheGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheGame
+//
+//  Created by Jorge Vazquez on 4/13/16.
+//  Copyright © 2016 Jorge Vazquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
